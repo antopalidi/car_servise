0e939f99441931053224dcbc014183952b139101
antopalidi <t.opalidisanna@gmail.com>
2022-07-23 18:32:52 +0200

    add buttons for sort

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :jobs
   resources :categories
   resources :workers
+
+  get "orders/filter/:filter" => "orders#index", as: :filtered_orders
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
