0fbc71d2f5bea0daef742ef3a8b1b8f9b200288d
antopalidi <t.opalidisanna@gmail.com>
2022-07-23 03:44:08 +0200

    add params to job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -65,6 +65,6 @@ class JobsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def job_params
-      params.require(:job).permit(:title)
+      params.require(:job).permit(:title, :category_id)
     end
 end
